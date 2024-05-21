7c58da63664ae41ed8fe918d1afbe0b1df680113
Alejandra <monicsalejandra@gmail.com>
2024-05-21 11:46:43 +0200

    it was corrected some functions, validData

--- JS/main.js
+++ JS/main.js
@@ -1,39 +1,38 @@
-//vamos a conectar lo que el placeholder nos de y buscar los valores.
+// ValidateForm function to validate form input fields
 function ValidateForm() {
-    let email = document.getElementById('InputEmail').value
-    let name = document.getElementById('InputName').value
-    let phone = document.getElementById('InputNumber').value
+    let email = document.getElementById('InputEmail').value;
+    let name = document.getElementById('InputName').value;
+    let phone = document.getElementById('InputNumber').value;
 
     if (email === "") {
-        alert("the field is required");
+        alert("The email field is required");
         return false;
-    } else if (!email.includes(`@`)) {
-        alert("the email is not valid");
+    } else if (!email.includes('@')) {
+        alert("The email is not valid");
         return false;
     }
 
     if (name === "") {
-        alert("the field is required");
+        alert("The name field is required");
         return false;
     }
 
     if (phone === "") {
-        alert("the field is required");
+        alert("The phone field is required");
         return false;
     }
 
     return true;
 }
 
-// tenemos un stamente donde dice si el correo es igual a vacio nos retornara false y nos enviara un alert que dice "the field is required" y seguira hasta el else donde dice si el correo no incluye una arroba entonces nos retornara fals0 y mostrara un alert donde dice que el "the email is not valid". y sucedera lo mismo con los demas if
-
+// ReadData function to read data from localStorage and display it in the table
 function ReadData() {
     let listPeople;
 
     if (localStorage.getItem('listPeople') === null) {
         listPeople = [];
     } else {
-        listPeople = JSON.parse(localStorage.getItem(listPeople))
+        listPeople = JSON.parse(localStorage.getItem('listPeople'));
     }
 
     let html = "";
@@ -42,28 +41,32 @@ function ReadData() {
         html += "<td>" + element.email + "</td>";
         html += "<td>" + element.name + "</td>";
         html += "<td>" + element.phone + "</td>";
-        html += '<td><button onclick="deleteData(' + index + ')" class= "btn btn-danger"> Delete data </button><button onclick="editData(' + index + ')" class= "btn btn-warning"> Editar data </button>';
+        html += '<td><button onclick="deleteData(' + index + ')" class="btn btn-danger">Delete</button>';
+        html += '<button onclick="editData(' + index + ')" class="btn btn-warning">Edit</button></td>';
         html += "</tr>";
     });
 
-    document.querySelector('#tableData').innerHTML = html;;
-
+    document.querySelector('#tableData tbody').innerHTML = html;
 }
 
-document.onload = ReadData();
+// Set window.onload to call ReadData when the page loads
+window.onload = function () {
+    ReadData();
+};
 
-function addData() {
-    if (ValidateForm === true) {
-        let email = document.getElementById('InputEmail').value
-        let name = document.getElementById('InputName').value
-        let phone = document.getElementById('InputNumber').value
+// AddData function to add data to localStorage
+function AddData() {
+    if (ValidateForm()) {
+        let email = document.getElementById('InputEmail').value;
+        let name = document.getElementById('InputName').value;
+        let phone = document.getElementById('InputNumber').value;
 
-        var listPeople;
+        let listPeople;
 
-        if (localStorage.getItem('listpeople' == null)) {
+        if (localStorage.getItem('listPeople') === null) {
             listPeople = [];
         } else {
-            listPeople = JSON.parse(localStorage.getItem(listPeople))
+            listPeople = JSON.parse(localStorage.getItem('listPeople'));
         }
 
         listPeople.push({
@@ -72,13 +75,32 @@ function addData() {
             phone: phone
         });
 
-        localStorage.setItem('listPeople', JSON.stringify());
+        localStorage.setItem('listPeople', JSON.stringify(listPeople));
 
         ReadData();
+
         document.getElementById('InputEmail').value = "";
         document.getElementById('InputName').value = "";
         document.getElementById('InputNumber').value = "";
     }
+}
+
+// deleteData function to remove data from localStorage
+function deleteData(index) {
+    let listPeople = JSON.parse(localStorage.getItem('listPeople'));
+    listPeople.splice(index, 1);
+    localStorage.setItem('listPeople', JSON.stringify(listPeople));
+    ReadData();
+}
+
+// editData function to edit data in the form and remove the original entry
+function editData(index) {
+    let listPeople = JSON.parse(localStorage.getItem('listPeople'));
+    let person = listPeople[index];
+    document.getElementById('InputEmail').value = person.email;
+    document.getElementById('InputName').value = person.name;
+    document.getElementById('InputNumber').value = person.phone;
+    deleteData(index);
 }
 
 
